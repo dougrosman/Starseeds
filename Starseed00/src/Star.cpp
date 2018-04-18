ef8ba1c5e66c795461ffa08800f8ad615060b78e
Doug Rosman <drosman@saic.edu>
2018-04-18 14:07:26 -0500

    Add Kinect example, create Star class

---
+++ Starseed00/src/Star.cpp
@@ -0,0 +1,8 @@
+//
+//  Star.cpp
+//  Starseed00
+//
+//  Created by Douglas Rosman on 4/18/18.
+//
+
+#include "Star.hpp"
---
+++ Starseed00/src/Star.hpp
@@ -0,0 +1,13 @@
+//
+//  Star.hpp
+//  Starseed00
+//
+//  Created by Douglas Rosman on 4/18/18.
+//
+
+#ifndef Star_hpp
+#define Star_hpp
+
+#include <stdio.h>
+
+#endif /* Star_hpp */
